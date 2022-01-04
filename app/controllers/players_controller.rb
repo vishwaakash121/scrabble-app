1f8fa804a22ee708d1eee83c4fc0898e2863a479
akash.vishwakarma <akash.vishwakarma>
2022-01-04 21:09:21 +0530

    updated controller action for save and update

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,18 +7,42 @@ class PlayersController < ApplicationController
 	end
 
 	def create
-		
+		@player = Player.new(player_params)
+		if @player.save!
+      flash.now[:notice] = 'Player get created!'
+		else
+      flash.now[:error] = 'Player can not be created!'
+		end
+		redirect_to root_path
 	end
 
 	def edit
 	end
 
 	def update
+		begin
+			@player.update!(player_params)
+			flash.now[:notice] = "Details got updated!"
+		rescue => e
+			e.record.errors.full_messages.each do |msg|
+				@player.errors.add(:base, msg)
+			end
+		end
+    respond_to do |format|
+      format.js
+    end
+	end
+
+	def show
 	end
 
 	private
 
 	def set_player
 		@player = Player.find(params[:id])
+	end
+
+	def player_params
+		params.require(:player).permit(Player.columns.map(&:name))
 	end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,16 @@ class Player < ApplicationRecord
 	has_many :match_players
 	has_many :matches, through: :match_players, source: :match
 	has_many :scores
-	belongs_to :best_match, class_name: "Match"
+	belongs_to :best_match, class_name: "Match", optional: true
+	validates :phone_no,:presence => true,
+							:numericality => true,
+							:length => { :minimum => 10, :maximum => 12 }
+
+	def highest_score
+		scores.find_by(match: best_match)&.score&.to_f
+	end
+
+	def get_opponent_player
+		best_match.players.where.not(id: self.id)&.last&.name
+	end
 end
